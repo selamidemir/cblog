65fd0da7a866aec84b21e07de2a159ec5030d697
Selami Demir <selamidemir@gmail.com>
2022-10-10 18:04:40 +0300

    ejs ayarlandı

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 
 const port = 3000;
 const app = express();
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.json({ id: 1, title: "Blog title", description: "Blog description" })
-})
+app.get("/", (req, res) => {
+  res.render('index.ejs');
+});
 
 app.listen(port, () => {
-    console.log('Cblog uygulaması ayağa kalktı.')
-})
+  console.log("Cblog uygulaması ayağa kalktı.");
+});
